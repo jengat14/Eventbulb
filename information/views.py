8f4cb95124b3d8bdf8ee45f9a05434c639fe726d
Jen Gates <jennygates14@gmail.com>
2022-02-24 20:56:32 +0000

    Add event page

--- information/views.py
+++ information/views.py
@@ -1,5 +1,9 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'information/home.html')
+
+def django_meetup(request):
+    return render(request, 'information/django_meetup.html')
