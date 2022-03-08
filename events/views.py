31e498b6a2a186ba4c2fa97c2b61e1413cd670fd
Jen Gates <jennygates14@gmail.com>
2022-03-08 21:09:26 +0000

    Add filter functionality

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
+from asyncio import events
 from django.shortcuts import render, get_object_or_404
 from events.models import Event
+from datetime import date, datetime
 
 # Create your views here.
 def details(request, id):
@@ -8,6 +10,33 @@ def details(request, id):
 
 
 def list(request):
-    events = Event.objects.all().order_by('datetime')
-    return render(request, 'events/list.html',{'events':events})
+    today = datetime.today()
+    filter_map = {
+        'title': 'title__icontains',
+        'is_free': 'cost__exact',
+    }
 
+    filters = {}
+    for key, value in request.GET.items():
+        filter_key = filter_map[key]
+        if value:
+            filters[filter_key] = value
+
+    events = Event.objects.filter(datetime__gte=today).filter(**filters).order_by('datetime')
+    return render(request, 'events/list.html', {'events': events})
+    
+    
+    
+    
+    
+    # query_params = request.GET    
+    # if 'title' in query_params and query_params['title'] != "":
+    #     events = Event.objects.filter(title__icontains=query_params['title'])
+    # else:
+    #     events = Event.objects.all()
+    # return render(request, 'events/list.html',{'events': events})
+
+    # events = Event.objects.all().order_by('datetime')
+    # return render(request, 'events/list.html',{'events':events})
+    
+   
